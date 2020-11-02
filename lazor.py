a76ba2261b32ad628728738a5bf7d65552307212
CameronCz <62724575+CameronCz@users.noreply.github.com>
2020-11-01 21:32:41 -0500

    Update lazor.py

--- lazor.py
+++ lazor.py
@@ -27,8 +27,13 @@ class Lazor_class(object):
                 Direction of lazor
         '''
         # Add direction and position to class
-        self.direction = direction
         self.initial_pos = initial_pos
+        self.direction = direction
+        # Comment out in future
+        print("Lazor Initial Position")
+        print(self.initial_pos)
+        print("Lazor Initial Direction")
+        print(self.direction)
 
 
 def read_input_file(board):
@@ -50,6 +55,17 @@ def read_input_file(board):
             Points lazor must intersect
 
     '''
+    # set up our lists and dictionaries
+    block_count = {'A': 0,
+                   'B': 0,
+                   'C': 0}
+    block_count_names = ['A', 'B', 'C']
+    # read added to end of lazor list to
+    # show which function variable is
+    # defined in
+    lazor_list_read = []
+    intersect_points = []
+    
     # open .bff file
     board_open = open(board, 'r')
     # read board file
@@ -94,8 +110,56 @@ def read_input_file(board):
         grid[grid_text_y] = grid_line
         grid_text_y = grid_text_y + 2
 
-    return grid
+    # index rest of .bff file to find
+    # block count, lazor, and interesct points
+
+    for line in board_open[grid_end:]:
+        # if the beginning of the line isn't in
+        # a block name, it must be lazor info
+        # or intersecting point info
+        if not line[0] in block_count_names:
+            if line[0] == 'L':
+                lazor_list_read.append(
+                    [tuple(map(int, line.replace('L', '').split()[s:s + 2]))
+                     for s in [0, 2]])
+            elif line[0] == 'P':
+                intersect_points.append(
+                    tuple(map(int, line.replace('P', '').split())))
+        # if line starts with block letter
+        # add it to the dictionary
+        if line[0] in block_count_names:
+            # add number of block type to dictionary
+            block_count[line[0]] = int(line[2])
+    print(grid)
+    print(block_count)
+    print(intersect_points)
+    print(lazor_list_read)
+    return grid, block_count, intersect_points, lazor_list_read
+
+def board_solver_process(board):
+    '''
+    Function that solvers the given puzzle
+
+    ** Parameters **
+            board: *string*
+                name of the .bff file we wish to solve
+
+    '''
+    # Read input file
+    grid, block_count, intersect_points, lazor_list_read = read_input_file(
+        board)
+
+    # Add lazor list to lazor class
+    for lazor_list_index in lazor_list_read:
+        Lazor_class(lazor_list_index[0], lazor_list_index[1])
+
+    # Make every possible combination of solutions
+
+    # sort solutions by most likely to work
+
+    # begin testing
+
+    # save solution
 
 if __name__ == '__main__':
-    read_input_file('mad_1.bff')
-#     board_solver('mad_1.bff')
+    board_solver_process('mad_1.bff')
