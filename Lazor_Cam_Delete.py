626e94264e767545c2d2109ca00c0a0c4f19b738
CameronCz <62724575+CameronCz@users.noreply.github.com>
2020-11-05 01:01:22 -0500

    Update Lazor_Cam_Delete.py

--- Lazor_Cam_Delete.py
+++ Lazor_Cam_Delete.py
@@ -5,60 +5,273 @@ Tianxin Zhang & Cameron Czerpak
 '''
 import numpy as np
 import time
-import scipy
-import matplotlib.pyplot as plt
-import itertools
+import random
 
 
 class Lazor_class(object):
     '''
+    Class for Lazor. Contains functions for how the lazor
+    interacts with different block types and the points
+    the lazor needs to cross
     '''
+
     def __init__(self, initial_pos, direction):
         '''
+        Initialize Lazor_class with given initial
+        position and associated direction
+        ** Parameters **
+            initial_pos: *typle* *int*
+                Initial position of lazor
+            direction: *tuple* *int*
+                Direction of lazor
         '''
         # Add direction and position to class
         self.initial_pos = initial_pos
         self.direction = direction
-        
+
+    def intersect_pts_remaining(self, grid, intersect_points):
+        '''
+        Determine how many intersection points haven't been
+        crossed after the lazor moves through the grid
+        ** Parameters **
+            grid: *list, list* *int*
+                Grid with all blocks (A,B,and/orC) placed
+            intersect_points: *list, tuple* *int*
+                xy positions of points the lazor must cross
+        ** Returns **
+            intersect_pts_notcrossed: *list, tuple* *int*
+                intersect_points not crossed by lazor
+        '''
+        # add intersection points to class
+        self.intersect_points = intersect_points
+        # determine where the lazor goes
+        lazor_moves = self.lazor_data(grid)[1]
+        # determine how many intersection points are left
+        # from the lazor's movement
+        intersect_pts_notcrossed = []
+        for k in intersect_points:
+            if k not in lazor_moves:
+                intersect_pts_notcrossed.append(k)
+        return intersect_pts_notcrossed
+
     def lazor_data(self, grid):
         '''
+        Determine how many intersection points haven't been
+        crossed after the lazor moves through the grid
+        ** Parameters **
+            grid: *list, list* *int*
+                Grid with all blocks (A,B,and/orC) placed
+        ** Returns **
+            block_intersect: *list, tuple* *int*
+                positions of blocks that the lazor intersected
+            lazor_moves: *list, tuple* *int*
+                positions on grid that the lazor intersected
         '''
+        # Create variable for lazor position and direction
+        # from class within the lazor data function
         lazor_pos = self.initial_pos
         direction = self.direction
-        
+
+        # Set up lists for block intersections and
+        # lazor movement
+        block_intersect = []
+        lazor_moves = []
+        lazor_moves.append(self.initial_pos)
+
         def in_array(lazor_pos, direction):
             '''
             Adapted from Weekly Challenge 7 Given code
-            "def pos_chk(x, y, nBlocks):"
             Validate if the coordinates specified (x and y)
-            are within the maze.
-
+            from lazor position are in the grid
             **Parameters**
-                x: *int*
-                    An x coordinate to check if it resides within the maze.
-                y: *int*
-                    A y coordinate to check if it resides within the maze.
-                nBlocks: *int*
-                    How many blocks wide the maze is.  Should be equivalent to
-                    the length of the maze (ie. len(maze)).
+                lazor_pos: *tuple*
+                    lazor position (x, y)
+                direction: *tuple*
+                    lazor dirction (x, y)
             **Returns**
-                valid: *bool*
-                    Whether the coordiantes are valid (True) or not (False).
+                True or False *bool*
+                    Whether the coordiantes are valid (True) or not (False)
             '''
             # https://www.flake8rules.com/rules/W503.html
             # Choosing current best practice
             # Does show error but anit-pattern does
             # too
-
+            # update lazor position x, and y with direction
             lazor_pos_x = lazor_pos[0] + direction[0]
             lazor_pos_y = lazor_pos[1] + direction[1]
+            # get x and y length of grid
             grid_x_len = len(grid[0])
             grid_y_len = len(grid)
+            # determine if lazor position is in grid
             return (lazor_pos_x >= 0 and lazor_pos_x < grid_x_len and
                     lazor_pos_y >= 0 and lazor_pos_y < grid_y_len)
-                    
+
+        def block_new(lazor_pos, direction):
+            '''
+            Determine the position of the next block
+            ** Parameters **
+                lazor_pos: *tuple* *int*
+                    lazor position (x, y)
+                direction: *tuple* *int*
+                    lazor dirction (x, y)
+
+            ** Returns **
+                block_1: *tuple* *int*
+                    next block on path
+            '''
+            # determine initial x0 and y0
+            x_0, y_0 = lazor_pos
+            # determine new x and y
+            x_1 = lazor_pos[0] + direction[0]
+            y_1 = lazor_pos[1] + direction[1]
+
+            # Determine next block location
+            x_1y_0 = x_1 % 2 + y_0 % 2
+            x_0y_1 = x_0 % 2 + y_1 % 2
+            if x_1y_0 != 0:
+                # create new block location
+                block_1 = (x_1, y_0)
+            elif x_0y_1 != 0:
+                # create new block location
+                block_1 = (x_0, y_1)
+            else:
+                return False
+            # Return new block
+            return block_1
+
+        def reflect_block(lazor_pos, direction, block_next):
+            '''
+            When the lazor hits a reflect block, this function
+            is called to change the lazor direction
+            ** Parameters **
+                lazor_pos: *tuple* *int*
+                    lazor position (x, y)
+                direction: *tuple* *int*
+                    lazor dirction (x, y)
+                block_next: *tuple* *int*
+                    next block on path
+
+            ** Returns **
+                direction: *tuple* *int*
+                    updated direction of lazor
+            '''
+            reflect_x = block_next[0] - lazor_pos[0]
+            reflect_y = block_next[1] - lazor_pos[1]
+
+            reflect_x_scale = 2 * reflect_x
+            reflect_y_scale = 2 * reflect_y
+
+            # update direction
+            direction = (direction[0] - reflect_x_scale,
+                         direction[1] - reflect_y_scale)
+            return direction
+
+        def refract_block(lazor_pos, direction, block_next):
+            '''
+            When the lazor hits a refract block, this function
+            is called to change the lazor direction by calling
+            the reflect_block function, and have the lazor
+            continue on its current path.
+            ** Parameters **
+                lazor_pos: *tuple* *int*
+                    lazor position (x, y)
+                direction: *tuple* *int*
+                    lazor dirction (x, y)
+                block_next: *tuple* *int*
+                    next block on path
+
+            ** Returns **
+                direction: *tuple* *int*
+                    updated direction of lazor
+            '''
+            initial_pos_new = (lazor_pos[0] + direction[0],
+                               lazor_pos[1] + direction[1])
+            # start a new lazor along the orgin direction
+            lazor_split = Lazor_class(initial_pos_new, direction)
+            lazor_moves_split = lazor_split.lazor_data(grid)
+            # get all the block and point crossed by the new lazor
+            for i in lazor_moves_split[0]:
+                block_intersect.append(i)
+            for i in lazor_moves_split[1]:
+                lazor_moves.append(i)
+            # reflect the current lazor
+            direction = reflect_block(lazor_pos, direction, block_next)
+            return direction
+
+        # Using function above, determine how the lazor moves
+        # through the grid, and intersection points which are
+        # crossed
+        while in_array(lazor_pos, direction):
+            # Determine next block and block type
+            block_next = block_new(lazor_pos, direction)
+            block_next_type = grid[block_next[1]][block_next[0]]
+            # Check block type anc call appropriate function
+            if block_next_type == 'A':
+                direction = reflect_block(lazor_pos, direction, block_next)
+            elif block_next_type == 'C':
+                direction = refract_block(lazor_pos, direction, block_next)
+            elif block_next_type == 'B':
+                # End lazor's movement due to hitting
+                # an opaque block
+                return block_intersect, lazor_moves
+            # If 'o', append block next due to it being
+            # a free space
+            elif block_next_type == 'o':
+                block_intersect.append(block_next)
+
+            # update the current point to the next point
+            lazor_pos = (lazor_pos[0] + direction[0],
+                         lazor_pos[1] + direction[1])
+            lazor_moves.append(lazor_pos)
+        return block_intersect, lazor_moves
+
+
+def solution_check(lazors, grid, intersect_points):
+    '''
+    For each lazor in lazors, check if there are any
+    intersection points that haven't been crossed
+    ** Parameters **
+        lazors: *object* *list*
+            lazor position at start of solving
+        grid: *list, list* *combination str and int*
+            Grid with all blocks (A,B,and/orC) placed
+        intersect_points: *list, tuple* *int*
+                xy positions of points the lazor must cross
+    ** Returns **
+        True or False *bool*
+            Whether block locations is a valid solution
+    '''
+
+    # add remaining intersect points to list
+    for each_lazor in lazors:
+        intersect_points = each_lazor.intersect_pts_remaining(
+            grid, intersect_points)
+    # if intersect_points is empty, all intersection points
+    # have been crossed. Return truex to end the code
+    if len(intersect_points) == 0:
+        return True
+    else:
+        return False
+
+
 def read_input_file(board):
     '''
+    Open the .bff file, and output data for solving
+    ** Parameters **
+        board: *.bff file*
+            input file with all game data
+    ** Returns **
+        grid: *list, list* *combination str and int*
+            Grid with only o and x filled in at correct points
+        block_count: *dict*
+            Block totals of each type to dictionary
+        intersect_points: *list*
+            list of points lazor must intersect for completion
+        lazors: *object* *list*
+            lazor position and direction at start of solving
+        o_locations: *list*
+            (x,y) locations 'o's on grid where A,B, and C can go
+
     '''
     # set up our lists and dictionaries
     block_count = {'A': 0,
@@ -114,7 +327,7 @@ def read_input_file(board):
         for letter in line:
             # Add o coordinate to list
             if letter == 'o':
-                o_locations.append((grid_text_x,grid_text_y))
+                o_locations.append((grid_text_x, grid_text_y))
             # put letter into correct size grid
             grid_line.append(letter)
             grid_line.append(0)
@@ -122,10 +335,9 @@ def read_input_file(board):
         # increment
         grid[grid_text_y] = grid_line
         grid_text_y = grid_text_y + 2
-    print(o_locations)
+
     # index rest of .bff file to find
     # block count, lazor, and interesct points
-
     for line in board_open[grid_end:]:
         # if the beginning of the line isn't in
         # a block name, it must be lazor info
@@ -143,23 +355,36 @@ def read_input_file(board):
         if line[0] in block_count_names:
             # add number of block type to dictionary
             block_count[line[0]] = int(line[2])
-    lazors = [Lazor_class(lazor_index[0], lazor_index[1]) for lazor_index in lazor_list_read]
+    lazors = []
+    for lazor_index in lazor_list_read:
+        lazors.append(Lazor_class(lazor_index[0], lazor_index[1]))
 
-    return (grid, block_count, intersect_points, lazors,
-            grid_x_len, grid_y_len, o_locations)
-            
+    return (grid, block_count, intersect_points, lazors, o_locations)
+
+
 def board_solver_process(board):
     '''
+    This is the main function for solving the board.
+    It calls read_input_file to read the board and inputs,
+    then it sets up the potential blocks to be placed.
+    Then blocks are randomly placed on the board and the
+    the solution is checked.
+    ** Parameters **
+        board: *.bff file*
+            input file with all game data
+    ** Returns **
+        boardsolution.txt *.txt file*
+            Writes solution to text file
     '''
+    # Start board timer
     t1 = time.time()
 
+    # Create board name for saving
     board_name = str(board)
     board_name = board_name.replace('.bff', '')
 
-    # grid, blocks, lazors, goal = read_input_file(board)
     (grid, block_count, intersect_points, lazors,
-        grid_x_len, grid_y_len, o_locations) = read_input_file(
-        board)
+        o_locations) = read_input_file(board)
 
     # Make list of blocks we can use
     block_list = []
@@ -167,21 +392,15 @@ def board_solver_process(board):
         if b == 'A':
             for ai in range(block_count.get('A')):
                 block_list.append('A')
-        if b == 'C':
+        elif b == 'C':
             for ai in range(block_count.get('C')):
                 block_list.append('C')
-        if b == 'B':
+        elif b == 'B':
             for ai in range(block_count.get('B')):
                 block_list.append('B')
 
-    ##################################
-    # Need to add a faster solving method
-    # place blocks where the lazor goes
-    # current code takes too long
-    #
-
+    # Originally tired every combination, but
     # intertools permutations is way too slow
-    # for i in itertools.permutations(o_locations[:3]):
     # for i in itertools.permutations(o_locations):
     # tries_list = []
     # Random actually works much faster
@@ -189,9 +408,6 @@ def board_solver_process(board):
     iterations = 1
     while unsolved:
         i = random.sample(o_locations, len(block_list))
-        # tries_list.append(i)
-        # if i in tries_list:
-        #     continue
 
         for j in range(len(block_list)):
             # print(j)
@@ -201,15 +417,20 @@ def board_solver_process(board):
             grid[i_y][i_x] = block_list[j]
 
         # We now have our grid that we want to test
-        #
-        solve = ######################### Add your code
+        solve = solution_check(lazors, grid, intersect_points)
+        # if solve, we have the grid that works
+        # Print the grid solution to the terminal
+        # Print the time and number of iterations
+        # Then create a txt file that saves the
+        # block locations similar to how they're
+        # shown in the input file
         if solve:
             print("Grid Solution")
             print(np.matrix(grid))
             t2 = time.time()
             print('Iterations = %s' % iterations)
             time_elapsed = t2 - t1
-            print('Time Elapsed = %s' % time_elapsed)
+            print('Time Elapsed = %s s' % time_elapsed)
             print("Solution saved as txt file")
             # Write solution file
             board_solution = board_name + "solution.txt"
@@ -233,7 +454,12 @@ def board_solver_process(board):
             i_y = block_xy[1]
             grid[i_y][i_x] = 'o'
         iterations = iterations + 1
-        
+
 
 if __name__ == '__main__':
+    board_solver_process("tiny_5.bff")
     board_solver_process("mad_1.bff")
+    board_solver_process("mad_4.bff")
+    board_solver_process("mad_7.bff")
+    board_solver_process("numbered_6.bff")
+    board_solver_process("yarn_5.bff")
